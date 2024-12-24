caa48e69316d6d53f85b649befe7ecc5a13b1d68
krishna <kyadav9711@gmail.com>
2024-12-24 12:51:04 +0530

    rating and review product by buyer

---
+++ services/ProductRateServices.jsx
@@ -0,0 +1,36 @@
+
+import { BASE_URL, getCookie } from "@/helper/common";
+import axios from "axios";
+
+export default class ProductRateServices {
+    // add rate service     
+    static async addRate(data) {
+        const token = getCookie("token");
+        const url = `${BASE_URL}/rating/reviews`
+        return axios.post(url, data, {
+            headers: {
+                Authorization: `Bearer ${token}`, // Set the token in the Authorization header
+            },
+        });
+    }
+    // Get rate service 
+    static async getRate(productDtlId) {
+        const token = getCookie("token");
+        const url = `${BASE_URL}/rating/reviews/${productDtlId}`
+        return axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${token}`, // Set the token in the Authorization header
+            },
+        });
+    }
+    // Upadte rate service 
+    static async UpdateRate(reviewId, data) {
+        const token = getCookie("token");
+        const url = `${BASE_URL}/rating/reviews/${reviewId}`
+        return axios.put(url, data, {
+            headers: {
+                Authorization: `Bearer ${token}`, // Set the token in the Authorization header
+            },
+        });
+    }
+}
